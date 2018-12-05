f04261e52d902a57fdf110494be467f7f3cc2a31
Roxana del Toro <rodeltor@microsoft.com>
2018-12-04 18:32:03 -0800

    Add new route

--- customer_charges.py
+++ customer_charges.py
@@ -7,6 +7,12 @@ def health():
     return 'all is wlll'
 
 # Accepts the charge processing results
-@app.route('/set-charges')
+@app.route('v1/set-charges')
 def post_charge_processing_results():
+    return
+
+
+# Retrieves a list of charges, each with an 'Approval' or 'Decline' state
+@app.route('/v1/get-charges/<partner_name>')
+def get_charge_processing_results(partner_name):
     return
